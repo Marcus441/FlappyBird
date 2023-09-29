c14e0ff5c46b44c0f09640009aad1c9a37486704
Marcus441 <psminchia4153@hotmail.com>
2023-09-29 12:44:45 +1000

    final changes

--- buzzer.js
+++ buzzer.js
@@ -1,35 +1,31 @@
-const pigpio = require('pigpio');
-const Gpio = pigpio.Gpio;
+const pigpio = require('pigpio');                           //include pigpio to interact with the GPIO
+const Gpio = pigpio.Gpio;                                   //include GPIO module              
 
-const outPin = 17;
+const outPin = 17;                                          //GPIO17 is connected to the buzzer                  
 
-const output = new Gpio(outPin, { mode: Gpio.OUTPUT });
+const output = new Gpio(outPin, { mode: Gpio.OUTPUT });     //use GPIO pin 17 as output                                                             
 
-function playTone(buzz) {
-  output.digitalWrite(0);
-  pigpio.waveClear();
+function playTone(buzz) {                                   //function to start the buzzer with a frequency and duration
+  output.digitalWrite(0);                                   //set output to low
+  pigpio.waveClear();                                       //clear all waveforms 
 
-  let waveform = [];
+  let waveform = [];                                        //set waveform as empty array
 
-  for (let x = 0; x < 20; x++) { //loop 20 times
-    if (x % 2 === 1) { //if x is odd (50% duty cycle)
+  for (let x = 0; x < 20; x++) {                            //loop 20 times
+    if (x % 2 === 1) {                                      //if x is odd (50% duty cycle)
       waveform.push({ gpioOn: outPin, gpioOff: 0, usDelay: buzz }); //pulse on
     } else {
       waveform.push({ gpioOn: 0, gpioOff: outPin, usDelay: buzz }); //pulse off
     }
   }
 
-  pigpio.waveAddGeneric(waveform);
-
-  let waveId = pigpio.waveCreate();
-
-  if (waveId >= 0) {
-    pigpio.waveTxSend(waveId, pigpio.WAVE_MODE_ONE_SHOT);
+  pigpio.waveAddGeneric(waveform);                           //add waveforms to the waveform array
+  let waveId = pigpio.waveCreate();                          //create complex waveforms
+  if (waveId >= 0) {                                         //if waveId is valid
+    pigpio.waveTxSend(waveId, pigpio.WAVE_MODE_ONE_SHOT);    //transmit waveform
   }
-
-  while (pigpio.waveTxBusy()) { }
-
+  while (pigpio.waveTxBusy()) { }                            //wait for all waveforms to be sent before continuing
   pigpio.waveDelete(waveId);
 }
 
-module.exports.playTone = playTone;
+module.exports.playTone = playTone;                          //export the function
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const buzzer = require('./buzzer.js');
-const express = require('express'); // import express
-const app = express(); // initialize express
-const server = require('http').createServer(app);
-const WebSocket = require('ws');
+const buzzer = require('./buzzer.js');                // import buzzer.js
+const express = require('express');                   // import express
+const app = express();                                // initialize express
+const server = require('http').createServer(app);     // create http server
+const WebSocket = require('ws');                      // import websocket
 
-const wss = new WebSocket.Server({ server: server });
+const wss = new WebSocket.Server({ server: server }); // create websocket server
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws) {        // when a client connects
   buzzer.playTone(500);
   console.log("a client has connected");
-  ws.send("welcome to flappy bird");
+  ws.send("welcome to flappy bird");                  // send a message to the client
 
-  ws.on('message', function incoming(message) {
-    if (message == 'gameover') {
-      ws.send('gameover');
-      buzzer.playTone(2000);
+  ws.on('message', function incoming(message) {       // when a client sends a message
+    if (message == 'gameover') {                      // if the message is gameover
+      ws.send('gameover');                            // send gameover to the client
+      buzzer.playTone(2000);                          // play a tone
     }
-    if (message == 'pass') {
-      ws.send('pass');
-      buzzer.playTone(1000);
+    if (message == 'pass') {                          // if the message is pass
+      ws.send('pass');                                // send pass to the client
+      buzzer.playTone(1000);                          // play a tone
     }
 
   })
